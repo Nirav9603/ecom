2cde65b319e9518f666f45be7f64d4b2d2f94aa8
Nirav9603 <niravmagatarapara111@gmail.com>
2022-09-09 16:00:47 +0530

    integration

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Switch } from 'react-router-dom'
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+import Home from './containers/home/Home';
+import Shop from './containers/shop/Shop';
+import About from './containers/about/About';
+import Services from './containers/services/Services';
+import Blog from './containers/blog/Blog';
+import Contact from './containers/contact/Contact';
+import Cart from './containers/cart/Cart';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header/>
+    <Switch>
+     <Route path={'/home'} exact component={Home}/>
+     <Route path={'/shop'} exact component={Shop}/>
+     <Route path={'/about'} exact component={About}/>
+     <Route path={'/services'} exact component={Services} />
+     <Route path={'/blog'} exact component={Blog}/>
+     <Route path={'/contact'} exact component={Contact}/>
+     <Route path={'/cart'} exact component={Cart}/>
+    </Switch>
+    <Footer/>
+    </>
   );
 }
 
---
+++ src/components/header/Header.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom'
+
+function Header(props) {
+    return (
+        <div>
+            <nav className="custom-navbar navbar navbar navbar-expand-md navbar-dark bg-dark" arial-label="Furni navigation bar">
+                <div className="container">
+                    <a className="navbar-brand" href="index.html">Furni<span>.</span></a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsFurni" aria-controls="navbarsFurni" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon" />
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarsFurni">
+                        <ul className="custom-navbar-nav navbar-nav ms-auto mb-2 mb-md-0">
+                            <li><NavLink className="nav-link scrollto active" to={"/home"}>Home</NavLink></li>
+                            <li><NavLink className="nav-link scrollto" to={'/shop'}>Shop</NavLink></li>
+                            <li><NavLink className="nav-link scrollto" to={'/about'}>About us</NavLink></li>
+                            <li><NavLink className="nav-link scrollto" to={'/services'}>Services</NavLink></li>
+                            <li><NavLink className="nav-link scrollto" to={'/blog'}>Blog</NavLink></li>
+                            <li><NavLink className="nav-link scrollto" to={'/contact'}>Contact us</NavLink></li>
+                        </ul>
+                        <ul className="custom-navbar-cta navbar-nav mb-2 mb-md-0 ms-5">
+                            <li><a className="nav-link" href="#"><img src="assets/images/user.svg" /></a></li>
+                            <li><NavLink className="nav-link" to={'/cart'}><img src="assets/images/cart.svg" /></NavLink></li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+
+    );
+}
+
+export default Header;
